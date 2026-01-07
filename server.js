ad947943d572613d4caae4240c1e11c11ffb61d6
Rushikesh Suryawanshi <rushisuryawanshi146@gmail.com>
2026-01-07 19:47:26 +0530

    Initial server setup

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import { Server } from 'socket.io'
 import http from 'http'
-import { Socket } from 'dgram';
+
 
 const app = express();
 
-const FRONTEND_URL = "https://chatbot-socketio.vercel.app";
+
 
 app.get('/', (req, res) => {
-    res.status(200).json({ 
-        status: 'OK', 
-        message: 'Socket.IO Server is running. Use a client to connect via WebSocket.' 
+    res.status(200).json({
+        status: 'OK',
+        message: 'Socket.IO Server is running. Use a client to connect via WebSocket.'
     });
 });
 
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: FRONTEND_URL,
+        origin: "http://localhost:5173",
         methods: ["GET", "PUT"]
     }
 })
@@ -32,7 +32,8 @@ io.on("connection", (Socket) => {
         console.log(`User ID :- ${Socket.id} joined room : ${data}`)
     })
 
-    Socket.on("send_message", (data) => {console.log("send message data", data)
+    Socket.on("send_message", (data) => {
+        console.log("send message data", data)
         Socket.to(data.room).emit("receive_message", data)
 
 
@@ -46,4 +47,6 @@ io.on("connection", (Socket) => {
 
 app.use(cors());
 
-module.exports = server;
+server.listen(1000, () => {
+    console.log("Server is running on port 1000");
+});
